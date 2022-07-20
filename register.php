c7752f37976b357252f780b4ebec9fc89d15588a
Kai <kaierikjesse@web.de>
2022-07-20 15:12:38 +0200

    Changed Layout for register.php

--- register.php
+++ register.php
@@ -9,46 +9,62 @@
 </head>
 
 <body>
-    <div class="center borderpadding">
-        <h2>Registrierung</h2>
 
-        <form action="includes/register.inc.php" method="post">
-            <?php
-            if (!isset($_GET['name'])) {
-                echo '<input type="text" name="name" placeholder="Benutzername" value="" autofocus><br>';
-            } else {
-                $tname = $_GET['name'];
-                echo '<input type="text" name="name" value="' . $tname . '" autofocus><br>';
-            }
-            ?>
-            <input type="password" name="password" placeholder="Passwort" value=""> <br>
-            <input type="password" name="password_again" placeholder="Passwort wdh." value=""> <br>
-            <button type="submit" name="register_submit">Best&aumltigen</button> <br>
-            <a class="col_blue" href="login.php">Zurück</a>
-        </form>
-        <?php
-        // Fehlermeldungen
-        isset($_GET['ms']) ? $message = $_GET['ms'] : $message = '';
-        if ($message !== '') {
-            switch ($message) {
-                case 'empty':
-                    echo '<p>Eingabefelder sind unvollständig</p>';
-                    break;
-                case 'even':
-                    echo '<p>Passwort stimmt nicht überein</p>';
-                    break;
-                case 'db';
-                    echo '<p>Fehler an der Datenbank. Bitte versuchen Sie es später erneut</p>';
-                    break;
-                case 'taken';
-                    echo '<p>Benutzer ist vergeben </p>';
-                    break;
-                case 'fail';
-                    echo '<p>Bitte versuchen Sie es später erneut</p>';
-                    break;
-            }
-        }
-        ?>
+    <div class="container mt-5">
+        <div class="row">            
+            <div class="col-md-6 text-center offset-md-3">
+                <h2>Registrierung</h2>
+                <form action="includes/register.inc.php" method="post">
+                    <div class="row">
+                        <div class="col-6 offset-3">
+                            <?php
+                            if (!isset($_GET['name'])) {
+                                echo '<input class="form-control mt-3 text-center" type="text" name="name" placeholder="Benutzername" autofocus>';
+                            } else {
+                                echo '<input class="form-control mt-3 text-center" type="text" name="name" value="' . $_GET['name'] . '" autofocus>';
+                            }
+                            ?>
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-6 offset-3">
+                            <input class="form-control text-center" type="password" name="password" placeholder="Passwort">
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col-6 offset-3">
+                            <input class="form-control text-center" type="password" name="password_again" placeholder="Passwort wdh.">
+                        </div>
+                    </div>
+                        <button class="btn btn-primary mt-2" type="submit" name="register_submit">Best&aumltigen</button>
+                </form>
+                <a class="col_blue" href="login.php">Zurück</a>
+                <br>
+                <?php
+                // Fehlermeldungen
+                isset($_GET['ms']) ? $message = $_GET['ms'] : $message = '';
+                if ($message !== '') {
+                    switch ($message) {
+                        case 'empty':
+                            echo '<p>Eingabefelder sind unvollständig</p>';
+                            break;
+                        case 'even':
+                            echo '<p>Passwort stimmt nicht überein</p>';
+                            break;
+                        case 'db';
+                            echo '<p>Fehler an der Datenbank. Bitte versuchen Sie es später erneut</p>';
+                            break;
+                        case 'taken';
+                            echo '<p>Benutzer ist vergeben </p>';
+                            break;
+                        case 'fail';
+                            echo '<p>Bitte versuchen Sie es später erneut</p>';
+                            break;
+                    }
+                }
+                ?>                
+            </div>
+        </div>
     </div>
 </body>
 
