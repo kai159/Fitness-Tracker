3704497f4b87b22043aeee79dbba99afc70f20d2
Kai <kaierikjesse@web.de>
2022-07-25 08:37:15 +0200

    Changed layout for training_edit

--- training_edit.php
+++ training_edit.php
@@ -8,7 +8,7 @@
 <html lang="de">
 
 <head>
-    <title>Alle Trainings</title>
+    <title>Training editieren</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/main.css">
     <script src="js/training_edit.js"></script>
@@ -17,31 +17,50 @@
 
 <body class="b_body">
     <div class="container text-center">
-        <h2 class="header">Alle Trainings</h2>
+        <h2 class="header">Training editieren</h2>
         <div class="row">
             <?php
-            include 'includes/functions.php';
-            $result = get_active_exercises_by_tid($_POST['tid']);
-            if ($result->num_rows > 0) {
-                echo'
-                <div class="table-responsive text-center mt-2">
-                    <table class="table table-bordered b_table"> 
-                    <tr> 
-                    <th>Name</th>
-                    <th>Entfernen</th>
-                    </tr>';
-                    
-                foreach($result as $item){
-                    echo'
-                    <tr id="' . $item['id'] . '">
-                    <td>' . $item['name'] . '</td>
-                    <td><button onClick="exercise_delete_submit(this.id)" 
-                    class="btn btn-secondary" id="' . $item['id'] . '_' . $_POST['tid'] . '">Löschen</button></td>
-                    </tr>';
+                include 'includes/functions.php';
+                $result_training = get_single_training_active($_GET['tid']);
+                if (!$result_training->num_rows > 0) {
+                    header('Location: /training_overview.php');
+                    exit();
                 }
-                echo'</table>';
-                $result_training = get_single_training_active($_POST['tid']);
+                $result = get_active_exercises_by_tid($_GET['tid']);
                 $row = $result_training->fetch_assoc();
+
+                // First block with image
+                echo '
+                <div class="row">
+                <div class="col-xxl-4 col-lg-4 order-first"></div>
+                <div class="col-xxl-4 col-lg-4 shadow-sm px-4 mt-3" style="min-width: 350px;">
+                <h3 onclick="extra_hidden()">' . $row['name'] . '</h3>
+                <p>' . $row['description'] . '</p> 
+                <img class="mb-4" style="width:270px; height:120px;" src="data:image/jpeg;base64,' . base64_encode($row['picture']) . '"/>
+                </div>';
+
+
+                // Table with delete options
+                if ($result->num_rows > 0) {
+                    echo'
+                    <div class="table-responsive text-center mt-2">
+                        <table class="table table-bordered b_table"> 
+                        <tr> 
+                        <th>Name</th>
+                        <th>Entfernen</th>
+                        </tr>';
+                        
+                    foreach($result as $item){
+                        echo'
+                        <tr id="' . $item['id'] . '">
+                        <td>' . $item['name'] . '</td>
+                        <td><button onClick="exercise_delete_submit(this.id)" 
+                        class="btn btn-secondary" id="' . $item['id'] . '_' . $_GET['tid'] . '">Löschen</button></td>
+                        </tr>';
+                    }
+                    echo'</table>';
+                }
+                // Options for editing
                 echo'
                 <div class="row">
                     <div class="col-xxl-6 col-lg-6 mt-3">        
@@ -62,8 +81,8 @@
                             <button class="btn btn-secondary btn-sm" type="submit" name="update_img">Ändern</button> <br>
                         </form>
                     </div>
-                    <div class="col-xxl-4 col-lg-6 mt-3"></div>
-                    <div class="col-xxl-4 col-lg-6 mt-3">
+                    <div class="col-xxl-4 col-lg-3 mt-3"></div>
+                    <div class="col-xxl-4 mt-3">
                         <form action="includes/training_edit.inc.php" method="post">
                             <label for="file">Komplettes Training löschen:</label><br>
                             <input type="hidden" name="id_tr" value="' . $row['id'] . '">
@@ -71,7 +90,6 @@
                         </form>
                     </div>
                 </div>';
-            }
             ?>
         </div>
     </div>
